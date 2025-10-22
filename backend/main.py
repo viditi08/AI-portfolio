ac664877c60ab73df9900623cb558342ec8cdc38
Viditi08 <viditivartak08@gmail.com>
2025-10-22 12:32:54 -0400

    Fix langchain import statement

--- backend/main.py
+++ backend/main.py
@@ -7,7 +7,7 @@ from dotenv import load_dotenv
 
 # LangChain Imports
 from langchain_community.document_loaders import DirectoryLoader, PyPDFLoader, TextLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import Chroma
 from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain_anthropic import ChatAnthropic
